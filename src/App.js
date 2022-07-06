3b0dc337dcaa056611050fb2e1945d8210dd62fc
HoCH_DS <caio_hoch@outlook.com>
2022-07-06 00:09:57 -0300

    Netflix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import Tmdb from './Tmdb';
 import './App.css';
+import MovieRow from './components/MovieRow';
+import FeaturedMove from './components/FeaturedMove';
+import Header from './components/Header'
 
-function App() {
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pengando o Featured
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+
+    }
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListiner = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+    window.addEventListener('scroll', scrollListiner);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListiner)
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+
+      <Header black={blackHeader} />
+
+      {featuredData &&
+        <FeaturedMove item={featuredData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://onlinegiftools.com/images/examples-onlinegiftools/netflix-stream-opaque.gif" alt="carregando" />
+        </div>
+      }
     </div>
   );
 }
-
-export default App;
